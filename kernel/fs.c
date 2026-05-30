1522008956acd0d3350864df17b938fa81a75486
YunMunJR <b20031020@gmail.com>
2026-05-30 15:07:28 +0800

    update again

--- kernel/fs.c
+++ kernel/fs.c
@@ -800,9 +800,9 @@ namexbase(char *path, int nameiparent, char *name, struct inode *base)
         }
         iunlockput(ip);
         if(target[0] == '/'){
-          ip = namexbase(target, 0, name, 0);
+          ip = namei(target);
         }else{
-          ip = namexbase(target, 0, name, linkbase);
+          ip = namei_base(target, linkbase);
         }
 
         if(ip == 0){
